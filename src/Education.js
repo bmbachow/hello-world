b672d5b50002cb7d55dfb0835ee13866843f7425
Alen Diaz <noricual@gmail.com>
2020-05-05 23:00:32 -0500

    style added

--- src/Education.js
+++ src/Education.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 function Education() {
   return (
     <div className="education">
       <h2>Education</h2>
       <ul>
-        <li>Thinkful University</li>
-        <li>School of Hard Knocks</li>
+      <li>Thinkful University</li>
+      <li>School of Hard Knocks</li>
       </ul>
     </div>
   );
